5cf17152cc3ee4fa5ed47c2e5ce6b6c93720e7a8
bartKuijper <bart.kuijper@ordina.nl>
2021-09-05 13:09:59 +0200

    added basic framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { animationFrameScheduler, BehaviorSubject, interval } from 'rxjs';
+import { delay, map, repeat, take, takeUntil, takeWhile, tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'graphs-rxjs';
+export class AppComponent implements OnInit {
+  points: string[] = [];
+
+  ball = new BehaviorSubject({ x: 0, y: 0 });
+  ball$ = this.ball.pipe(
+    tap(console.log),
+    map(cor => `translate(${cor.x}px, ${-cor.y}px)`)
+  )
+
+
+  ngOnInit() {
+    interval(90, animationFrameScheduler).pipe(
+      takeWhile(x => x < 1000),
+      map(x => ({ x: ((Math.cos(x)) * 200), y: ((Math.sin(x)) * 200) })),
+      tap(cor => this.ball.next(cor)),
+      map(cords => `translate(${cords.x}px, ${-cords.y}px)`),
+      tap(x => this.points = [...this.points, x]),
+      tap(x => this.points.length > 500 ? this.points = [] : null),
+    ).subscribe()
+  }
+
+
 }
